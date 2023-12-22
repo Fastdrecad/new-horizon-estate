6402f432c7aee9b35aec1af72db222aed666ca7c
Fastdrecad <andrijas.micun@gmail.com>
2023-12-22 07:43:02 +0100

    Update navbar and make profile page private

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-5">
@@ -19,9 +22,9 @@ export default function Navbar() {
           />
           <FaSearch className="text-slate-600" />
         </form>
-        <ul className="flex gap-4 ">
+        <ul className="flex items-center gap-4 font-medium text-lg">
           <Link to="/home">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline  text-slate-700 hover:underline">
               Home
             </li>
           </Link>
@@ -30,8 +33,16 @@ export default function Navbar() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className=" text-slate-700 hover:underline">Sign in</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-9 w-9 object-cover"
+                src={currentUser.avatar}
+                alt="avatar"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
