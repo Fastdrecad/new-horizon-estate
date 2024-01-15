a9ecfd8eafa4678b1f51473af5aa9f9d6887ee3f
Fastdrecad <andrijas.micun@gmail.com>
2024-01-15 19:41:18 +0100

    Incorporate modal for sending message to owner, and update the carousel on the home page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   FaBath,
   FaBed,
   FaChair,
   FaMapMarkerAlt,
   FaParking,
-  FaShare,
-} from "react-icons/fa";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import SwiperCore from "swiper";
-import "swiper/css/bundle";
-import { Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Contact from "../components/Contact";
+  FaShare
+} from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import SwiperCore from 'swiper';
+import 'swiper/css/bundle';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Contact from '../components/Contact';
+import ContactOwner from '../components/ContactOwner';
 
 export default function Listing() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -23,7 +26,7 @@ export default function Listing() {
   const [copied, setCopied] = useState(false);
   const [contact, setContact] = useState(false);
   const params = useParams();
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector(state => state.user);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -49,30 +52,31 @@ export default function Listing() {
 
   return (
     <main>
-      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
-        <p className="text-center my-7 text-2xl">
+        <p className='text-center my-7 text-2xl'>
           Sorry, we couldn&apos;t find that listing!
         </p>
       )}
       {listing && !loading && !error && (
         <>
           <Swiper navigation>
-            {listing.imageUrls.map((url) => (
+            {listing.imageUrls.map(url => (
               <SwiperSlide key={url}>
                 <div
-                  className="h-[500px]"
+                  className='h-[500px]'
                   style={{
-                    background: `url(${url}) center no-repeat `,
+                    background: `url(${url})  no-repeat `,
                     backgroundSize: `cover`,
+                    backgroundPosition: '50% 65%'
                   }}
                 ></div>
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer shadow-3xl">
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer shadow-3xl'>
             <FaShare
-              className="text-slate-500"
+              className='text-slate-500'
               onClick={() => {
                 navigator.clipboard.writeText(window.location.href);
                 setCopied(true);
@@ -83,68 +87,77 @@ export default function Listing() {
             />
           </div>
           {copied && (
-            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
               Link copied!
             </p>
           )}
-          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
-            <p className="text-2xl font-semibold">
+          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+            <p className='text-2xl font-semibold'>
               {listing.name} - $
               {listing.offer
-                ? listing.regularPrice.toLocaleString("en-US")
-                : listing.discountPrice.toLocaleString("en-US")}
-              {listing.type === "rent" && "/month"}
+                ? listing.regularPrice.toLocaleString('en-US')
+                : listing.discountPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && '/month'}
             </p>
-            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
-              <FaMapMarkerAlt className="text-green-700" />
+            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+              <FaMapMarkerAlt className='text-green-700' />
               {listing.address}
             </p>
-            <div className="flex gap-4">
-              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                {listing.type === "rent" ? "For Rent" : "For Sale"}
+            <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
               </p>
               {listing.offer && (
-                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                   ${+listing.discountPrice} Discount
                 </p>
               )}
             </div>
-            <p className="text-slate-800 ">
-              <span className="font-semibold text-black">Description: </span>
+            <p className='text-slate-800 '>
+              <span className='font-semibold text-black'>Description: </span>
               {listing.description}
             </p>
-            <ul className=" text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
-              <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaBed className="text-lg" />
+            <ul className=' text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+              <li className='flex items-center gap-1 whitespace-nowrap'>
+                <FaBed className='text-lg' />
                 {listing.bedrooms > 1
                   ? `${listing.bedrooms} beds`
                   : `${listing.bedrooms} bed`}
               </li>
-              <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaBath className="text-lg" />
+              <li className='flex items-center gap-1 whitespace-nowrap'>
+                <FaBath className='text-lg' />
                 {listing.bedrooms > 1
                   ? `${listing.bedrooms} baths`
                   : `${listing.bedrooms} bath`}
               </li>
-              <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaParking className="text-lg" />
-                {listing.parking ? "Parking" : "No Parking"}
+              <li className='flex items-center gap-1 whitespace-nowrap'>
+                <FaParking className='text-lg' />
+                {listing.parking ? 'Parking' : 'No Parking'}
               </li>
-              <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaChair className="text-lg" />
-                {listing.furnished ? "Furnished" : "Unfurnished"}
+              <li className='flex items-center gap-1 whitespace-nowrap'>
+                <FaChair className='text-lg' />
+                {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
 
             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
-                onClick={() => setContact(true)}
-                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+                onClick={() => {
+                  setIsModalOpen(!isModalOpen);
+                  setContact(true);
+                }}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
               >
                 Contact owner
               </button>
             )}
-            {contact && <Contact listing={listing} />}
+            {contact && (
+              <ContactOwner
+                listing={listing}
+                isOpen={isModalOpen}
+                setIsOpen={setIsModalOpen}
+              />
+            )}
           </div>
         </>
       )}
